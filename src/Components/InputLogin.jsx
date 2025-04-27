96e8a6250c7d1db774bb6983d4cfb5851eac8150
Mafikul Islam <mdmafikulislam5320@gmail.com>
2025-04-27 20:03:40 +0600

    Input Error set and Succesfull Set

--- src/Components/InputLogin.jsx
+++ src/Components/InputLogin.jsx
@@ -1,13 +1,26 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React from 'react';
+import React, { useState } from 'react';
 import { auth } from '../UserLogin';
 
 const InputLogin = () => {
+    const [Error, setError] = useState("");
+    const [Succesfull, setSuccesfull] = useState(false)
     const HandelSubmit = (event) => {
         event.preventDefault()
         const Email = event.target.email.value
-        const Password = event.target.password.value
-        createUserWithEmailAndPassword(auth, Email, Password).then(res => console.log(res)).catch(error=> console.log(error))
+        const Password = event.target.password.value;
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        if (passwordRegex.test(Password) === false) {
+            setError("Your Password is Not strong...");
+            return;
+        }
+
+        setError("")
+        setSuccesfull(false)
+        createUserWithEmailAndPassword(auth, Email, Password).then(res => {
+            console.log(res)
+            setSuccesfull(true)
+        }).catch(error => setError(error.message))
     }
     return (
         <div className="hero ">
@@ -24,6 +37,12 @@ const InputLogin = () => {
                                 <button className="btn btn-neutral mt-4">Login</button>
                             </fieldset>
                         </form>
+                        {
+                            <p className='text-red-500'>{Error}</p>
+                        }
+                        {
+                            Succesfull && <p className='text-green-500 font-extralight'>User Create Succesfull</p>
+                        }
                     </div>
                 </div>
             </div>
